dda722fec19b4a4907df7a92502bef279c357ec7
Team8271 <31601375+Team8271@users.noreply.github.com>
2025-02-18 10:41:39 -0700

    Updated Servo Methods | Added Testing
    Method setServoPosition(Servo servo, int degree) has been written out for first time, it is untested.
    
    Method servoOpposite(int degree) has been written out for the first time, it is untested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveDev/SwerveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveDev/SwerveTeleOp.java
@@ -22,19 +22,12 @@ public class SwerveTeleOp extends LinearOpMode {
       double mainThrottle = .2+(gamepad1.right_trigger*0.8);
       boolean resetFCD = gamepad1.dpad_up;
 
-      //Need to set servo position based on joysticks
-      //Power also needs to be set through joysticks / throttle
-
       double gamepadRadians = Math.atan2(lateralControl, axialControl);
       double gamepadHypot = Range.clip(Math.hypot(lateralControl, axialControl), 0, 1);
       double robotRadians = -robot.odometer.getZ();
       double targetRadians = gamepadRadians + robotRadians;
       double lateral = Math.sin(targetRadians)*gamepadHypot;
       double axial = Math.cos(targetRadians)*gamepadHypot;
-
-      telemetry.addLine("Lateral: " + lateral);
-      telemetry.addLine("Axial: " + axial); //up down
-      telemetry.update();
 
       /* Servo position is going to be:
        * more left/right based on lateral
@@ -43,26 +36,49 @@ public class SwerveTeleOp extends LinearOpMode {
        */
 
       
+      //If servo position is set at 200 starting at 0
+      //It should go to 20 ticks
 
+      //Tell it to go to 200
+      if(gamepad1.dpadUp){
+        setServoPosition(robot.testServo, 200);
+      }
 
-
-
-
-
+      //Tell it to go to 0
+      if(gamepad1.dpadRight){
+        setServoPosition(robot.testServo, 200);
+      }
+      
+      //Update telemetry
+      if(gamepad1.a){
+        telemetry.addLine("Test Servo Position" + robot.testServo.getPosition());
+        telemetry.update();
+      }
+      
     }
   }
 
   //Method to set Servo position to desired position with minimal rotation
   public void setServoPosition(Servo servo, int position){
-    //If the distance to position is best achieved by 0-180 range (rather than 180-360)
-    if(Math.abs(servo.getPosition()-position) <= Math.abs(servo.getPosition()-***)){
-
+    //If degree inputed is closer than the opposite of that degree 
+    if(Math.abs(servo.getPosition()-position) <= Math.abs(servo.getPosition()-degreeOpposite(position)){
+      servo.setPosition(position);
+    }
+    //If Opposite degree is closer
+    else{
+      servo.setPosition(degreeOpposite(position);
     }
   }
 
-  //Method to get opposite rotation of servo so that the wheel still faces same direction (90 - 270)
+  //Method for opposite rottion of a Servo ie(90 & 270 keep wheel facing same direction)
   public int degreeOpposite(int degree){
-    
+    //For a positive int
+    if(degree >= 0){
+      return degree += 180;
+    }
+    //For a negative int
+    else{
+      return degree -= 180;
+    }
   }
-
 }
